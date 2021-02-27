a7629e6bcec13a2247e9b9a5563bb06a436817a4
student <t.king@f5.com>
2021-02-27 17:09:04 +0000

    Initial Jenkinsfile project commit

---
+++ src/main/java/com/kingttx/JenkinsfileDemo/JenkinsfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kingttx.JenkinsfileDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsfileDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingttx/JenkinsfileDemo/JenkinsfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingttx.JenkinsfileDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
